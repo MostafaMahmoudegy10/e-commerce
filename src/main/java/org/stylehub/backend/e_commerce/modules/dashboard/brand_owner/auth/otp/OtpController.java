e55d68a7dd30a6cc0924648e501a4765c2c59340
MostafaMahmoudegy10 <mostafa.mahmoudegy10@gmail.com>
2026-04-12 19:30:49 +0200

    add authorization on otp

--- src/main/java/org/stylehub/backend/e_commerce/modules/dashboard/brand_owner/auth/otp/OtpController.java
+++ src/main/java/org/stylehub/backend/e_commerce/modules/dashboard/brand_owner/auth/otp/OtpController.java
@@ -1,6 +1,7 @@
 package org.stylehub.backend.e_commerce.modules.dashboard.brand_owner.auth.otp;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.stylehub.backend.e_commerce.modules.dashboard.brand_owner.auth.otp.dto.*;
 import org.stylehub.backend.e_commerce.modules.dashboard.brand_owner.auth.otp.service.OtpService;
@@ -8,6 +9,7 @@ import org.stylehub.backend.e_commerce.modules.dashboard.brand_owner.auth.otp.se
 @RestController
 @RequestMapping("api/v1/public/otp")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('BRAND_OWNER')")
 public class OtpController {
 
     private final OtpService otpService;
